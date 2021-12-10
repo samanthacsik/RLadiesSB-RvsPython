56c8afc8936b83fe54ade15df0fc0f9abe53453e
samanthacsik <samanthacsik@gmail.com>
2021-12-10 15:43:50 -0800

    started creating base R tidyverse equivalent

---
+++ baseR_equivalents.R
@@ -0,0 +1,112 @@
+# Some dplyr vs. base R comparisions: https://cran.r-project.org/web/packages/dplyr/vignettes/base.html
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            ~~
+##                        STEP 1: LOAD PACKAGES & DATA                      ----
+##                                                                            ~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#..........................load packages.........................
+
+# no packages to load, we're using base R!
+
+#..........................read in data..........................
+
+sweaters <- read.csv("ugly_sweaters.csv")
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            ~~
+##                        STEP 2: WRANGLE/PROCESS DATA                      ----
+##                                                                            ~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#................filter hs_tf for 'Yes' responses................
+
+sweaters_yes_option1 <- subset(sweaters, hs_tf == "Yes")
+sweaters_yes_option2 <- sweaters[which(sweaters$hs_tf == "Yes"), , drop = FALSE]
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                            count sweater colors                          ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#..................tidy colors (1 color per row).................
+
+# separate by ','
+sweater_colors_list1 <- strsplit(sweaters_yes_option1$colors, split = c(", "))
+sweater_colors1 <- data.frame(sweater = rep(sweaters_yes_option1$sweater, sapply(sweater_colors_list1, length)), colors = unlist(sweater_colors_list1))
+
+# separate by 'and'
+sweater_colors_list2 <- strsplit(sweater_colors1$colors, split = c(" and "))
+sweater_colors2 <- data.frame(sweater = rep(sweater_colors1$sweater, sapply(sweater_colors_list2, length)), colors = unlist(sweater_colors_list2))
+
+#..............group by sweaterID and count colors...............
+
+color_counts <- aggregate(cbind(count = colors) ~ sweater, 
+          data = sweater_colors2, 
+          FUN = function(x){NROW(x)})
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                      count sweater description words                     ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#..................tidy descriptions (1 word per row).................
+
+# separate by ','
+sweater_words_list <- strsplit(sweaters_yes_option1$image_desc, split = c(" "))
+sweater_words <- data.frame(sweater = rep(sweaters_yes_option1$sweater, sapply(sweater_words_list, length)), words = unlist(sweater_words_list))
+
+#..............group by sweaterID and count colors...............
+
+word_counts <- aggregate(cbind(count = words) ~ sweater, 
+                          data = sweater_words, 
+                          FUN = function(x){NROW(x)})
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                  join dfs                                ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+all_counts <- merge(x = color_counts, y = word_counts, by = "sweater", all = TRUE)
+all_counts[is.na(all_counts)] = 0
+
+# ADDRESS THIS
+all_counts$count.x <- as.numeric(all_counts$count.x)
+colors <- all_counts$count.x
+words <- all_counts$count.y
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            ~~
+##                           STEP 3: VISUALIZE DATA                         ----
+##                                                                            ~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#....................create basic scatterplot....................
+
+# NEED TO FIX (currently getting an error when using all_counts df for plotting)
+simple_plot <- plot(words ~ jitter(colors, 1), 
+                    pch = 19, cex = 1.5, col = "gray48")
+
+simple_plot
+
+#..............create slightly fancier scatterplot...............
+
+fancy_plot <- plot(words ~ jitter(colors, 1), 
+                  pch = 19, cex = 1.5, col = "gray48",
+                  main = "Relationship between the number of colors and length of description for ugly holiday sweaters")
+
+# fancy_plot <- ggplot(tidy_counts, aes(x = num_colors, y = num_words)) +
+#   geom_jitter(size = 3, alpha = 0.5) +
+#   scale_x_continuous(breaks = seq(from = 2, to = 8, by = 2)) +
+#   geom_smooth(method = lm, se = TRUE, size = 0.5, color = "black") +
+#   labs(x = "Number of colors on sweater", 
+#        y = "Number of words in sweater description",
+#        title = str_wrap("Relationship between the number of colors and length of description for ugly holiday sweaters", 60)) +
+#   theme_classic()
+
+fancy_plot
